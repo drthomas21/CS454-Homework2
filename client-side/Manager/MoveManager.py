b87d1cc9a4f6e5848fca4777dac9a14f21881d92
David Thomas <david.thomas214@yahoo.com>
2015-10-27 00:11:45 -0700

    Adding latest changes: Private chat (beta) moving remote chracters (beta)

---
+++ client-side/Manager/MoveManager.py
@@ -0,0 +1,32 @@
+from Models3D.Characters.PandaCharacter import PandaCharacter
+from Models3D.Characters.RalphCharacter import RalphCharacter
+from Models3D.Characters.VechileCharacter import VechileCharacter
+from Network.models.PositionConnectionModel import PositionConnectionModel
+
+class MoveManager:
+    def __init__(self, World):
+        self.Player = World.Character
+        self.World = World
+        self.PositionConnectionModel = PositionConnectionModel(World.CharacterManager.moveCharacter)
+        World.ServerConnection.setupConnectionModel(self.PositionConnectionModel)
+        self.actions = []
+    
+    def appendAction(self,left,right,forward,pos):
+        self.actions.append([left,right,forward,pos])
+        
+    def flushActions(self):
+        actions = self.actions
+        self.actions = []
+        return actions
+    
+    def sendMoves(self,task):
+        if self.World.stopSendingMovement:
+            return None
+        
+        if not self.World.bypassServer:
+            for moves in self.flushActions():
+                pos = moves[3]
+                self.PositionConnectionModel.sendPos(str(pos[0])+","+str(pos[1])+","+str(pos[2]))
+        else:
+            self.flushActions()
+        return task.again
